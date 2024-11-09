0edf9ee92c6d9bc86c7a1cf956a94a9ea9699d43
kirill <w_shkolenko@mail.ru>
2024-11-10 01:57:44 +0300

    Added logging

--- ml/lifespan.py
+++ ml/lifespan.py
@@ -3,20 +3,23 @@ import pickle
 import torch
 import whisper
 import imagebind
-
+from loguru import logger
 from transformers import GPT2Tokenizer, GPT2LMHeadModel
 
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
+logger.debug("loading wisper")
 whisper_model = whisper.load_model("tiny")
 whisper_model.eval()
 whisper_model.to(device)
 
+logger.debug("loading imagebind")
 imagebind_model = imagebind.model.imagebind_huge(True)
 imagebind_model.eval()
 imagebind_model.to(device)
 
+logger.debug("loading bert")
 tokenizer = GPT2Tokenizer.from_pretrained("sberbank-ai/rugpt3large_based_on_gpt2")
 model = GPT2LMHeadModel.from_pretrained("sberbank-ai/rugpt3large_based_on_gpt2")
 model.to(device)
